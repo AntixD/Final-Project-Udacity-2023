01cf611cb2db727a0ae643cdd13fe92f3034b6b4
AntixD <antonio.dedic123@gmail.com>
2023-01-11 19:43:56 +0100

    Final commit

---
+++ src/client/index.js
@@ -0,0 +1,29 @@
+import { checkForName } from './js/nameChecker'
+import { handleSubmit } from './js/formHandler'
+
+import './styles/resets.scss'
+import './styles/footer.scss'
+import './styles/form.scss'
+import './styles/header.scss'
+import './styles/footer.scss'
+import './styles/base.scss'
+
+export {
+    checkForName,
+    handleSubmit
+   }
+
+console.log(checkForName);
+
+
+// Check that service workers are working
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker.register("/service-worker.js");
+  });
+}
+
+
+
+
+  
